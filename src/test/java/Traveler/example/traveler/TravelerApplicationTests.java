495f439169b7a8a5facdfd6a1d4f3f889deaa05d
soop <kimdaesoop@hotmail.com>
2020-02-21 00:02:42 +0900

    first commit

---
+++ src/test/java/Traveler/example/traveler/TravelerApplicationTests.java
@@ -0,0 +1,13 @@
+package Traveler.example.traveler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
